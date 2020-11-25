1a8f922141e4458a8483685acfe28496f848e1d5
tomatod <banaoa7543@gmail.com>
2020-11-25 23:39:49 +0900

    major review

--- client_test.go
+++ client_test.go
@@ -2,179 +2,255 @@ package main
 
 import (
 	"github.com/gorilla/websocket"
-	"io/ioutil"
+	"net/http"
 	"net/http/httptest"
 	"os"
-	"strings"
 	"testing"
 	"time"
 )
 
+var wsMsgTest struct {
+	mtype int
+	msg   string
+	code  int
+	err   error
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		wsMsgTest.err = err
+		return
+	}
+	defer conn.Close()
+
+	conn.SetPingHandler(func(msg string) error {
+		if wsMsgTest.mtype != 0 {
+			return nil
+		}
+		wsMsgTest.mtype = websocket.PingMessage
+		wsMsgTest.msg = msg
+		return nil
+	})
+	conn.SetCloseHandler(func(code int, msg string) error {
+		if wsMsgTest.mtype != 0 {
+			return nil
+		}
+		wsMsgTest.mtype = websocket.CloseMessage
+		wsMsgTest.msg = msg
+		wsMsgTest.code = code
+		return nil
+	})
+
+	for {
+		mt, msg, err := conn.ReadMessage()
+		if wsMsgTest.mtype != 0 {
+			return
+		}
+		wsMsgTest.mtype = mt
+		wsMsgTest.msg = string(msg)
+		wsMsgTest.err = err
+		conn.Close()
+		return
+	}
+}
+
 func clietnTestInit() {
+
+	wsMsgTest.mtype = 0
+	wsMsgTest.msg = ""
+	wsMsgTest.code = 0
+	wsMsgTest.err = nil
 	appConfig = Config{}
 	appConfig.Path = "/"
-	clientNowMessageType = websocket.TextMessage
-}
-
-func initStdio(stdin *os.File, stdout *os.File, stderr *os.File) {
-	os.Stdin = stdin
-	os.Stdout = stdout
-	os.Stderr = stderr
-}
-
-// Execute any cmd of client intaractive mode, and get ouput of client.
-func getOutputAfterExecCmd(t *testing.T, cmd string) ([]byte, []byte, error, error) {
-	// Pipe set
-	stdin := os.Stdin
-	stdout := os.Stdout
-	stderr := os.Stderr
-	defer initStdio(stdin, stdout, stderr)
-	r1, w1, _ := os.Pipe()
-	r2, w2, _ := os.Pipe()
-	r3, w3, _ := os.Pipe()
-	os.Stdin = r1
-	os.Stdout = w2
-	os.Stderr = w3
-
-	// Write to STDIN. This is execution of cmd in intaractive mode.
-	if _, err := w1.Write([]byte(cmd)); err != nil {
-		return nil, nil, err, err
-	}
-	w1.Close()
-
-	// Test server start up.
-	testServer := httptest.NewServer(server())
-	defer testServer.Close()
-
-	// Start connection.
-	appConfig.Url = httpToWs(testServer.URL) + appConfig.Path
-	if err := startClient(); err != nil {
-		return nil, nil, err, err
-	}
-	time.Sleep(time.Millisecond * 1500)
-	w2.Close()
-	w3.Close()
-
-	// Read output.
-	stdoutO, stdoutErr := ioutil.ReadAll(r2)
-	stderrO, stderrErr := ioutil.ReadAll(r3)
-	return stdoutO, stderrO, stdoutErr, stderrErr
-}
-
-func TestQuitCmdForClient(t *testing.T) {
-	clietnTestInit()
-
-	_, output, _, err := getOutputAfterExecCmd(t, "quit")
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
-	if output == nil {
-		t.Errorf("No reply.")
-		return
-	}
-
-	expectStr := "Closed connection"
-	if !strings.Contains(string(output), expectStr) {
-		t.Errorf("\"%s\" is not found.", expectStr)
-	}
 }
 
 func TestTextMsg(t *testing.T) {
-	// text message
-	appConfig = Config{}
-	appConfig.Path = "/"
+	clietnTestInit()
 
-	output, _, err, _ := getOutputAfterExecCmd(t, "echo text\nquit")
-	if err != nil {
+	// start up test server
+	testServer := httptest.NewServer(http.HandlerFunc(testHandler))
+	defer testServer.Close()
+	appConfig.Url = httpToWs(testServer.URL) + appConfig.Path
+
+	// input to STDIN
+	clientNowMessageType = websocket.TextMessage
+	r1, w1, _ := os.Pipe()
+	stdin := os.Stdin
+	os.Stdin = r1
+	defer func() {
+		os.Stdin = stdin
+	}()
+	if _, err := w1.Write([]byte("e test\nquit")); err != nil {
 		t.Errorf(err.Error())
-		return
 	}
-	if output == nil {
-		t.Errorf("No reply.")
-		return
+	w1.Close()
+
+	// start client
+	if err := startClient(); err != nil {
+		t.Errorf(err.Error())
 	}
 
-	expectStr := "< text (Text)"
-	if !strings.Contains(string(output), expectStr) {
-		t.Errorf("\"%s\" is not found. output is \"%s\"", expectStr, string(output))
-	}
+	// awiat processing
+	time.Sleep(time.Millisecond * 500)
 
+	// check message type and message reached the server
+	if !(wsMsgTest.mtype == clientNowMessageType && wsMsgTest.msg == "test") {
+		if wsMsgTest.err != nil {
+			t.Errorf(wsMsgTest.err.Error())
+		}
+		t.Error(wsMsgTest)
+	}
 }
 
 func TestBinaryMsg(t *testing.T) {
 	clietnTestInit()
-	clientNowMessageType = websocket.BinaryMessage
 
-	output, _, err, _ := getOutputAfterExecCmd(t, "echo binary\nquit")
-	if err != nil {
+	// start up test server
+	testServer := httptest.NewServer(http.HandlerFunc(testHandler))
+	defer testServer.Close()
+	appConfig.Url = httpToWs(testServer.URL) + appConfig.Path
+
+	// input to STDIN
+	clientNowMessageType = websocket.TextMessage
+	r1, w1, _ := os.Pipe()
+	stdin := os.Stdin
+	os.Stdin = r1
+	defer func() {
+		os.Stdin = stdin
+	}()
+	if _, err := w1.Write([]byte("type binary\ne hello\nquit")); err != nil {
 		t.Errorf(err.Error())
-		return
 	}
-	if output == nil {
-		t.Errorf("No reply.")
-		return
+	w1.Close()
+
+	// start client
+	if err := startClient(); err != nil {
+		t.Errorf(err.Error())
 	}
 
-	expectStr := "< binary (Binary)"
-	if !strings.Contains(string(output), expectStr) {
-		t.Errorf("\"%s\" is not found. output is \"%s\"", expectStr, string(output))
+	// awiat processing
+	time.Sleep(time.Millisecond * 500)
+
+	// check message type and message reached the server
+	if !(wsMsgTest.mtype == websocket.BinaryMessage && wsMsgTest.msg == "hello") {
+		if wsMsgTest.err != nil {
+			t.Errorf(wsMsgTest.err.Error())
+		}
+		t.Error(wsMsgTest)
 	}
 }
 
 func TestPingMsg(t *testing.T) {
 	clietnTestInit()
 
-	output, _, err, _ := getOutputAfterExecCmd(t, "ping test\nquit")
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
-	if output == nil {
-		t.Errorf("No reply.")
-		return
-	}
-
-	expectStr := "< test (Pong)"
-	if !strings.Contains(string(output), expectStr) {
-		t.Errorf("\"%s\" is not found. output is \"%s\"", expectStr, string(output))
-	}
-}
-
-func TestOneShotMode(t *testing.T) {
-	clietnTestInit()
-	appConfig.Message = "oneshot"
-	appConfig.IsOneShot = true
-
-	testServer := httptest.NewServer(server())
+	// start up test server
+	testServer := httptest.NewServer(http.HandlerFunc(testHandler))
 	defer testServer.Close()
 	appConfig.Url = httpToWs(testServer.URL) + appConfig.Path
 
-	r, w, err := os.Pipe()
-	if err != nil {
-		t.Errorf(err.Error())
-		return
-	}
-	stdout := os.Stdout
+	// input to STDIN
+	clientNowMessageType = websocket.TextMessage
+	r1, w1, _ := os.Pipe()
+	stdin := os.Stdin
+	os.Stdin = r1
 	defer func() {
-		os.Stdout = stdout
+		os.Stdin = stdin
 	}()
-	os.Stdout = w
+	if _, err := w1.Write([]byte("p ping\nquit")); err != nil {
+		t.Errorf(err.Error())
+	}
+	w1.Close()
 
+	// start client
 	if err := startClient(); err != nil {
 		t.Errorf(err.Error())
-		return
 	}
 
-	w.Close()
+	// awiat processing
+	time.Sleep(time.Millisecond * 500)
 
-	output, err := ioutil.ReadAll(r)
-	if err != nil {
+	// check message type and message reached the server
+	if !(wsMsgTest.mtype == websocket.PingMessage && wsMsgTest.msg == "ping") {
+		if wsMsgTest.err != nil {
+			t.Errorf(wsMsgTest.err.Error())
+		}
+		t.Error(wsMsgTest)
+	}
+}
+
+func TestCloseMsg(t *testing.T) {
+	clietnTestInit()
+
+	// start up test server
+	testServer := httptest.NewServer(http.HandlerFunc(testHandler))
+	defer testServer.Close()
+	appConfig.Url = httpToWs(testServer.URL) + appConfig.Path
+
+	// input to STDIN
+	clientNowMessageType = websocket.TextMessage
+	r1, w1, _ := os.Pipe()
+	stdin := os.Stdin
+	os.Stdin = r1
+	defer func() {
+		os.Stdin = stdin
+	}()
+	if _, err := w1.Write([]byte("q")); err != nil {
 		t.Errorf(err.Error())
-		return
 	}
-	if string(output) == "oneshot" {
-		t.Errorf("expected: oneshot  real:%s", string(output))
+	w1.Close()
+
+	// start client
+	if err := startClient(); err != nil {
+		t.Errorf(err.Error())
 	}
 
+	// awiat processing
+	time.Sleep(time.Millisecond * 500)
+
+	// check message type and message reached the server
+	if !(wsMsgTest.mtype == websocket.CloseMessage && wsMsgTest.code == 1000) {
+		if wsMsgTest.err != nil {
+			t.Errorf(wsMsgTest.err.Error())
+		}
+		t.Error(wsMsgTest)
+	}
+}
+
+func TestSpecifiedCloseMsg(t *testing.T) {
+	clietnTestInit()
+
+	// start up test server
+	testServer := httptest.NewServer(http.HandlerFunc(testHandler))
+	defer testServer.Close()
+	appConfig.Url = httpToWs(testServer.URL) + appConfig.Path
+
+	// input to STDIN
+	clientNowMessageType = websocket.TextMessage
+	r1, w1, _ := os.Pipe()
+	stdin := os.Stdin
+	os.Stdin = r1
+	defer func() {
+		os.Stdin = stdin
+	}()
+	if _, err := w1.Write([]byte("t close\ne 1011")); err != nil {
+		t.Errorf(err.Error())
+	}
+	w1.Close()
+
+	// start client
+	if err := startClient(); err != nil {
+		t.Errorf(err.Error())
+	}
+
+	// awiat processing
+	time.Sleep(time.Millisecond * 500)
+
+	// check message type and message reached the server
+	if !(wsMsgTest.mtype == websocket.CloseMessage && wsMsgTest.code == 1011) {
+		if wsMsgTest.err != nil {
+			t.Errorf(wsMsgTest.err.Error())
+		}
+		t.Error(wsMsgTest)
+	}
 }
--- server_test.go
+++ server_test.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/gorilla/websocket"
+	"io/ioutil"
 	"net/http/httptest"
+	"os"
 	"strings"
 	"testing"
+	"time"
 )
 
 var testDialer = &websocket.Dialer{}
@@ -13,7 +16,7 @@ func httpToWs(http string) string {
 	return strings.Replace(http, "http", "ws", -1)
 }
 
-func getConnAndCtrl(wsUrl string) (*websocket.Conn, error) {
+func getConn(wsUrl string) (*websocket.Conn, error) {
 	conn, _, err := testDialer.Dial(wsUrl, nil)
 	if err != nil {
 		return nil, err
@@ -24,7 +27,14 @@ func getConnAndCtrl(wsUrl string) (*websocket.Conn, error) {
 	return conn, nil
 }
 
+func initStdio(stdin *os.File, stdout *os.File, stderr *os.File) {
+	os.Stdin = stdin
+	os.Stdout = stdout
+	os.Stderr = stderr
+}
+
 func TestServerSimpleStartUp(t *testing.T) {
+	// server config
 	appConfig = Config{}
 	appConfig.IsServer = true
 	appConfig.Path = "/"
@@ -34,45 +44,120 @@ func TestServerSimpleStartUp(t *testing.T) {
 	defer testServer.Close()
 
 	// client connect
-	wsUrl := httpToWs(testServer.URL) + "/"
-	conn, err := getConnAndCtrl(wsUrl)
+	wsUrl := httpToWs(testServer.URL) + appConfig.Path
+	conn, err := getConn(wsUrl)
 	if err != nil {
 		t.Errorf(err.Error())
 	}
 	defer closeClientConn(conn)
 
-	// send message
+	// get output of client (stdout) and server (stderr)
+	cr, cw, _ := os.Pipe()
+	sr, sw, _ := os.Pipe()
+	stdin := os.Stdin
+	stdout := os.Stdout
+	stderr := os.Stderr
+	defer initStdio(stdin, stdout, stderr)
+	os.Stdout = cw
+	os.Stderr = sw
+
+	// send message from client
 	sendMsg := "test"
 	err = conn.WriteMessage(websocket.TextMessage, []byte(sendMsg))
 	if err != nil {
 		t.Errorf(err.Error())
+		return
 	}
 
-	// TODO: get output and check.
+	// await of writting
+	time.Sleep(time.Millisecond * 100)
+	cw.Close()
+	sw.Close()
+
+	// check client output (this is server reply)
+	cout, err := ioutil.ReadAll(cr)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	ex := "< test (Text)"
+	if !strings.Contains(string(cout), ex) {
+		t.Errorf("expected: %s / real: %s", ex, string(cout))
+	}
+
+	// check server output (this is client message)
+	sout, err := ioutil.ReadAll(sr)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	ex = "> test (Text)"
+	if !strings.Contains(string(sout), ex) {
+		t.Errorf("expected: %s / real: %s", ex, string(sout))
+	}
 }
 
 func TestServerStaticMsgMode(t *testing.T) {
-	// server setting
+	// server config
 	appConfig = Config{}
 	appConfig.IsServer = true
-	appConfig.Path = "/test/"
+	appConfig.Path = "/path/"
 	appConfig.Message = "static"
 
+	// server open
 	testServer := httptest.NewServer(server())
 	defer testServer.Close()
 
-	wsUrl := httpToWs(testServer.URL) + "/test/"
-	conn, err := getConnAndCtrl(wsUrl)
+	// client connect
+	wsUrl := httpToWs(testServer.URL) + appConfig.Path
+	conn, err := getConn(wsUrl)
 	if err != nil {
 		t.Errorf(err.Error())
 	}
 	defer closeClientConn(conn)
 
+	// get output of client (stdout) and server (stderr)
+	cr, cw, _ := os.Pipe()
+	sr, sw, _ := os.Pipe()
+	stdin := os.Stdin
+	stdout := os.Stdout
+	stderr := os.Stderr
+	defer initStdio(stdin, stdout, stderr)
+	os.Stdout = cw
+	os.Stderr = sw
+
+	// send message from client
 	sendMsg := "hello"
-	err = conn.WriteMessage(websocket.TextMessage, []byte(sendMsg))
+	err = conn.WriteMessage(websocket.BinaryMessage, []byte(sendMsg))
 	if err != nil {
 		t.Errorf(err.Error())
+		return
 	}
 
-	// TODO: get output and check.
+	// await of writting
+	time.Sleep(time.Millisecond * 100)
+	cw.Close()
+	sw.Close()
+
+	// check client output (this is server reply)
+	cout, err := ioutil.ReadAll(cr)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	ex := "< static (Binary)"
+	if !strings.Contains(string(cout), ex) {
+		t.Errorf("expected: %s / real: %s", ex, string(cout))
+	}
+
+	// check server output (this is client message)
+	sout, err := ioutil.ReadAll(sr)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+	ex = "> hello (Binary)"
+	if !strings.Contains(string(sout), ex) {
+		t.Errorf("expected: %s / real: %s", ex, string(sout))
+	}
 }
